2f29c6dae6895488f6827d222cbc0fb99428df5b
Naguib5 <naguibnaguib2018@gmail.com>
2023-09-21 19:51:20 +0300

    change  theme  and create notes app

---
+++ lib/views/Notes_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Notes_view extends StatelessWidget {
+  const Notes_view({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
