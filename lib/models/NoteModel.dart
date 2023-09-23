b1c1b49e7f186c7b31dce409eca7aaf7ef44e254
Naguib5 <naguibnaguib2018@gmail.com>
2023-09-24 01:13:38 +0300

    create note model

---
+++ lib/models/NoteModel.dart
@@ -0,0 +1,7 @@
+class NoteModel {
+  NoteModel({required this.title, required this.subtitle, required this.date, required this.color});
+  final String title;
+  final String subtitle;
+  final String date ;
+  final int color;
+}
